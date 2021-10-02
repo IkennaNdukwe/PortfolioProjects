d79cd61ba713dd70b00ee4b896f2f527924cbcff
IkennaNdukwe <91786168+IkennaNdukwe@users.noreply.github.com>
2021-10-02 13:44:03 +0100

    Update covid-vaccinations.sql

--- covid-vaccinations.sql
+++ covid-vaccinations.sql
@@ -1,3 +1,12 @@
+/*
+Covid-19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Link to dataset: https://ourworldindata.org/covid-deaths
+MySQL 8.0 was used for this exploration.
+*/
+
+-- create 'vaccinations' table in the 'covid' database
+
 drop table if exists covid.vaccinations
     
 create table covid.vaccinations(
