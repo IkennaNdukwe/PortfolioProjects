dcde3332a0f28e18320291b80d2e676dd3a03931
IkennaNdukwe <91786168+IkennaNdukwe@users.noreply.github.com>
2021-10-02 13:42:13 +0100

    Update covid-deaths.sql

--- covid-deaths.sql
+++ covid-deaths.sql
@@ -1,3 +1,13 @@
+/*
+Covid-19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Link to dataset: https://ourworldindata.org/covid-deaths
+
+MySQL 8.0 was used for this exploration.
+*/
+
+-- create 'deaths' table in the 'covid' database
+
 drop table if exists covid.deaths;
 
 create table covid.deaths(
@@ -29,6 +39,7 @@ create table covid.deaths(
 	weekly_hosp_admissions_per_million text
     );
   
+-- load data into 'deaths' table 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/owid-covid-deaths.csv'
 INTO TABLE covid.deaths
 FIELDS TERMINATED BY ','
@@ -65,44 +76,52 @@ IGNORE 1 ROWS
 
 
 -- validate record count 
+
 select count(*) from covid.deaths
 
 -- view records 
+
 select * from covid.deaths
 
 -- Select fields for exploration 
+
 select location, date, total_cases, new_cases, total_deaths, population 
 from covid.deaths
 order by location, str_to_date(date, '%m/%d/%Y')
 
 -- total cases vs total deaths 
--- death_rate shows likelihood of dying if covid is contracted per country 
+-- death_rate shows likelihood of a person dying if covid is contracted per country - Nigeria, in this context
+
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as 'death_rate'
 from covid.deaths
-where location = "Nigeria"
--- where total_deaths is null
+where location = "Nigeria" -- can be changed as desired
 
 -- total cases vs population 
--- infection_rate shows percentage of population infected by the corona virus 
+-- infection_rate shows percentage of population infected with covid in your country
+
 select location, date, population, total_cases, (cast(total_cases as float)/population)*100 as 'infection_rate'
 from covid.deaths
 where location = "Nigeria"
 -- where location like '%states%'
 
--- countries with highest infection rate per to population
+-- countries with highest infection rate compared to population
+
 select location, population, max(total_cases) as 'max_total_cases', max((cast(total_cases as float)/population))*100 as 'percentage_population_infected'
 from covid.deaths
 group by location, population 
 order by percentage_population_infected desc 
 
--- countries with highest death rate per to population
+-- countries with highest death count per to population
+
 select location, population, max(total_deaths) as 'total_death_count'
 from covid.deaths
 where iso_code not like '%owid%' -- or where continent is not null 
 group by location 
 order by total_death_count desc 
 
+-- breaking things down by continent
 -- categorizing death count per continent 
+
 select continent, sum(new_deaths) as 'total_death_count'
 from covid.deaths
 where continent is not  null 
@@ -110,6 +129,7 @@ group by continent
 order by total_death_count desc 
 
 -- global numbers by date 
+
 select date, sum(new_cases) as 'daily_case_count_world', sum(new_deaths) as 'daily_death_count_world', (cast(sum(new_deaths) as float)/sum(new_cases))*100.00 as 'death_rate'
 from covid.deaths
 where continent is not null 
@@ -117,7 +137,8 @@ group by str_to_date(date, '%m/%d/%Y')
 order by str_to_date(date, '%m/%d/%Y'), daily_case_count_world
 
 -- global death rate summary
+
 select sum(new_cases) as 'daily_case_count_world', sum(new_deaths) as 'daily_death_count_world', (cast(sum(new_deaths) as float)/sum(new_cases))*100.00 as 'death_rate'
 from covid.deaths
 where continent is not null 
-order by str_to_date(date, '%m/%d/%Y'), daily_case_count_world
+
